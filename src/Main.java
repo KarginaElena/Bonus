4a9c2aa8cfe6a376c90a3295cddab6a9ecc90d06
Elena <len.kargina2010@yandex.ru>
2023-09-13 10:06:08 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int bonus = 20;
+        int price = 13676;
+        int mile = price / bonus;
+
+        System.out.println("За поездку вы получите " + mile + " миль(ю/и) ");
+    }
+}
